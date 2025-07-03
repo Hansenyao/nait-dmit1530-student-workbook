e1181f44c6de935263fa132b0b70cb85970aaa51
Hansenyao <yyfsingler@163.com>
2025-07-02 22:48:55 -0600

    update module-2/17 - Multi-Level Navigation

--- module-2/17 - Multi-Level Navigation/js/main.js
+++ module-2/17 - Multi-Level Navigation/js/main.js
@@ -1,3 +1,3 @@
-document.querySelector(".toggle-btn").addEventListener("click", () => {
+document.querySelector(".toggle-btn").addEventListener("click",()=>{
     document.querySelector(".collapsed").classList.toggle("expanded");
 })
